a83d9c6dce4ff9c04f676d8b5a4351c7d5dc09bf
mss38 <mss38k@gmail.com>
2022-09-22 20:49:22 +0600

    edit video and revalidate

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -5,17 +5,19 @@ export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:9000",
   }),
-  tagTypes: ["Videos"],
+  tagTypes: ["Videos", "Video", "ReletedVideos"],
   endpoints: (builder) => ({
     getVideos: builder.query({
       query: () => "/videos",
       keepUnusedDataFor: 600,
       providesTags: ["Videos"],
     }),
+
     getVideo: builder.query({
       query: (videoId) => `/videos/${videoId}`,
+      providesTags: (result, error, arg) => [{ type: "Video", id: arg }],
     }),
-    // ?title_like=css&title_like=tailwind&_limit=4
+
     getReletedVideos: builder.query({
       query: ({ id, title }) => {
         const tags = title.split(" ");
@@ -23,18 +25,39 @@ export const apiSlice = createApi({
         const queryString = `/videos?${likes.join("&")}&_limit=4`;
         return queryString;
       },
+      providesTags: (result, error, arg) => [{ type: "ReletedVideo", id: arg.id }],
     }),
+
     addVideo: builder.mutation({
       query: (data) => ({
         url: `/videos`,
         method: "POST",
         body: data,
       }),
-      invalidatesTags: ["Videos"]
+      invalidatesTags: ["Videos"],
+    }),
+    
+    editVideo: builder.mutation({
+      query: ({ id, data }) => ({
+        url: `/videos/${id}`,
+        method: "PATCH",
+        body: data,
+      }),
+      invalidatesTags: (result, error, arg) => [
+        "Videos",
+        { type: "Video", id: arg.id },
+        { type: "ReletedVideo", id: arg.id },
+      ],
     }),
   }),
 });
 
 console.log(apiSlice);
 
-export const { useGetVideosQuery, useGetVideoQuery, useGetReletedVideosQuery, useAddVideoMutation } = apiSlice;
+export const {
+  useGetVideosQuery,
+  useGetVideoQuery,
+  useGetReletedVideosQuery,
+  useAddVideoMutation,
+  useEditVideoMutation,
+} = apiSlice;
